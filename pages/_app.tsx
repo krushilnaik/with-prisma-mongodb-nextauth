1e82165ac687a7de5d9e680c11eb8083619f0f53
Krushil Naik <krushilnaik96@gmail.com>
2022-03-01 23:46:19 -0500

    Bug fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
+import { SessionProvider } from "next-auth/react";
+import { ApolloProvider } from "@apollo/client";
+
+import client from "@lib/apollo";
 import "@styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
 	return (
-		<div className="App">
-			<Component {...pageProps} />
-		</div>
+		<ApolloProvider client={client}>
+			<SessionProvider session={session}>
+				<div className="App">
+					<Component {...pageProps} />
+				</div>
+			</SessionProvider>
+		</ApolloProvider>
 	);
 }
 
